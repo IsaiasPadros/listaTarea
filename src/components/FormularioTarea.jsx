b7de91544ab9130eb11a9d5a669d06b0c9257b30
unknown <isaiaspadros2@gmail.com>
2023-07-18 16:50:54 -0300

    Se agrego la funcion de CrearListaTareas

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,16 +1,33 @@
 import { Form, Button } from "react-bootstrap";
 import ListaTareas from "./ListaTareas.jsx";
+import { useState } from "react";
 
 const FormularioTarea = () => {
+  const [tarea, setTarea] = useState("");
+  const [listaTareas, setListaTareas] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //guardar la tarea en el array listaTareas
+    setListaTareas([...listaTareas, tarea]);
+    //limpiar el value del input
+    setTarea("");
+  };
+
   return (
     <>
-    <Form>
-      <Form.Group className="mb-3 d-flex" controlId="tarea">
-        <Form.Control type="text" placeholder="ingrese una tarea" />
-        <Button variant="primary">Agregar</Button>
-      </Form.Group>
-    </Form>
-    <ListaTareas></ListaTareas>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3 d-flex" controlId="tarea">
+          <Form.Control
+           type="text" 
+          placeholder="ingrese una tarea"   
+          value={tarea}
+          onChange={(e) => setTarea(e.target.value)}
+           />
+          <Button variant="primary">Agregar</Button>
+        </Form.Group>
+      </Form>
+      <ListaTareas propsListaTareas={listaTareas}></ListaTareas>
     </>
   );
 };
